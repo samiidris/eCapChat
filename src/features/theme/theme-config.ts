fbd133edcfb255c311a92ddcfdfa94f68bc0fdca
samiidris <134462943+samiidris@users.noreply.github.com>
2024-07-23 06:29:24 -0500

    Update theme-config.ts

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,8 +1,8 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "eCapChat";
+export const AI_DESCRIPTION = "Welcome to eCapChat, your intelligent assistant designed to boost productivity and streamline your tasks with cutting-edge AI technology. How can I help you today?";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format. If a question is related to Information Technology give best effort but if the answer is probably best taken from the internal IT team then refer the user to email help@ecapitaladvisors.com
 
 You have access to the following functions:
 1. create_img: You must only use the function create_img if the user asks you to create an image.`;
